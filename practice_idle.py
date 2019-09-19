d712a47172b72a200836debd0aee00b5114bf20d
Elizabeth Fuenzalida <efuenzalida52@gmail.com>
2019-09-19 13:59:55 -0700

    Fixed Type() issue, added additional comments
    Fixed Type() in script so that it will actually print on shell.
    Lengthed and re-organized my code examples.
    added some additonal comments to explain examples further.

--- practice_idle.py
+++ practice_idle.py
@@ -3,17 +3,23 @@
 print(7*7)
 # after the first line above is ran it will print the following 'Hello'
 print('Hello')
+print() #creating a line breaks
 print()
+
 ###############################
 ## Using variables in python ##
 ###############################
-number = 7
-print(number)
+number = 7 #creating variables
 string = 'Hello World!'
-print(string)
-type(number) #type() tells you what data type the parameter is  
 
+print(number)
+print(type(number)) #type() tells you what data type the parameter is
+print()
+print()
+print((string) + ' is a ...')
+print(type(string))
 #python is dynamically typed language which means you can overwrite
 #previously defined variables with a completely different data type.
 string = 77
-
+print ('now the variable string is a...')
+print (type(string))
