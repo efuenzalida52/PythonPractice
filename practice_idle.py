b52892b0ce5182ca08df7b2f0612f1986acd7d9d
efuenzalida52 <efuenzalida52@gmail.com>
2019-09-18 23:11:38 -0700

    Updating Python Pratice file to contain variables
    Added variables to this practice file and played around with type().
    Added comments to remind me of dynamic versus static programming.

--- practice_idle.py
+++ practice_idle.py
@@ -1,5 +1,19 @@
 # testing out a simple python command.
 # This will print 7*7 in the shell when ran.
 print(7*7)
-# after the first line if ran it will print the following
+# after the first line above is ran it will print the following 'Hello'
 print('Hello')
+print()
+###############################
+## Using variables in python ##
+###############################
+number = 7
+print(number)
+string = 'Hello World!'
+print(string)
+type(number) #type() tells you what data type the parameter is  
+
+#python is dynamically typed language which means you can overwrite
+#previously defined variables with a completely different data type.
+string = 77
+
