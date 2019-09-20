592a538a6ccd2bd15ccbca5a1f873ab68cba003a
Elizabeth Fuenzalida <efuenzalida52@gmail.com>
2019-09-19 21:36:09 -0700

    started praticing with locial operators
    started refreshing in logical operators.

---
+++ logicalOperators.py
@@ -0,0 +1,9 @@
+#Practice with logical operators
+#logical operators can be used in if statements
+
+not True      # is False
+not False     # is True
+
+not 2 < 3     
+not 3 > 1    
+not 4 == 3    
