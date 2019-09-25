aa8828e1b377901befbcf8b4eb915eb1c6defafc
Elizabeth Fuenzalida <efuenzalida52@gmail.com>
2019-09-24 17:50:34 -0700

    Started TicTac Project
    Started workign on tic tac project

--- MiniProjects/TicTacToeProjects/TicTacToe.py
+++ MiniProjects/TicTacToeProjects/TicTacToe.py
@@ -1,2 +1,11 @@
 #Python Tic Tac Toe
 
+board =["  " for i in range(9)]
+
+def print_board():
+    #could do this in for loop
+    row1 = "|{}|{}|{}|".format(board[0],board[1],board[2])
+    row1 = "|{}|{}|{}|".format(board[3],board[4],board[5])
+    row1 = "|{}|{}|{}|".format(board[6],board[7],board[8])
+
+    print()
