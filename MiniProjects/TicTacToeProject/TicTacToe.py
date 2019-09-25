56cf1e931fe7cd91a7159b46d7b56287a3259ae3
Elizabeth Fuenzalida <efuenzalida52@gmail.com>
2019-09-25 15:14:50 -0700

    Rough finished Tic Tac Toe Project
    A working tic tac toe game. Code could ebe updated to be more neat/
    efficent.

--- MiniProjects/TicTacToeProject/TicTacToe.py
+++ MiniProjects/TicTacToeProject/TicTacToe.py
@@ -1,11 +1,71 @@
 #Python Tic Tac Toe
 
-board =["  " for i in range(9)]
-
+board =[" " for i in range(9)]
 def print_board():
-    #could do this in for loop
     row1 = "|{}|{}|{}|".format(board[0],board[1],board[2])
-    row1 = "|{}|{}|{}|".format(board[3],board[4],board[5])
-    row1 = "|{}|{}|{}|".format(board[6],board[7],board[8])
+    row2 = "|{}|{}|{}|".format(board[3],board[4],board[5])
+    row3 = "|{}|{}|{}|".format(board[6],board[7],board[8])
 
+#could do this in for loop
     print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+#player move section
+def player_move(icon):
+    if icon == "X":
+        number = 1
+    elif icon == "O":
+        number = 2
+
+    print("Player {}'s turn!".format(number))
+    
+    choice= int(input("Place your 'x' or 'o' by entering (1-9): ").strip())
+    if board[choice -1] == " ":     #-1 from player choice due to indexing
+        board[choice -1] = icon
+    else:
+        print()
+        print("Sorry, that space is taken!")
+        # need to implement players turn not being skipped
+
+
+#determine when a player wins
+def victory(icon):
+     if (board[0]==icon and board[1]==icon and board[2]==icon)or\
+        (board[3]==icon and board[4]==icon and board[5]==icon)or\
+        (board[6]==icon and board[7]==icon and board[8]==icon)or\
+        (board[0]==icon and board[3]==icon and board[6]==icon)or\
+        (board[1]==icon and board[4]==icon and board[7]==icon)or\
+        (board[2]==icon and board[5]==icon and board[8]==icon)or\
+        (board[0]==icon and board[4]==icon and board[8]==icon)or\
+        (board[2]==icon and board[4]==icon and board[6]==icon):
+        return True
+     else:
+        return False
+
+#determine when there is a tie
+def draw():
+    if "  " not in board:
+        return True
+    else:
+        return False
+        
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+    if victory("X"):
+        print("X wins!")
+        break
+    elif draw():
+        print("Draw!")
+    player_move("O")
+    if victory("O"):
+        print_board()
+        print("O wins!")
+        break
+    elif draw():
+        print("Draw!")
+        break
